de11b728ce04c03dfe9504e30d83175702a31006
David Matalik <david.matalik@posteo.de>
2021-10-25 08:37:15 +0200

    Implement toggle done functionality for Task

--- src/model/taskmodel.js
+++ src/model/taskmodel.js
@@ -1,5 +1,18 @@
-class Task {
+import { Item } from './itemmodel.js'
 
+class Task extends Item {
+  constructor(title) {
+    super(title)
+    this._done = false
+  }
+
+  get done() {
+    return this._done
+  }
+
+  toggleDone() {
+    this._done = !this._done
+  }
 }
 
 export { Task }
